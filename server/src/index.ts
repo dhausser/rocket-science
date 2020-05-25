3276c180c71b03067feff6faaa49140cfa9bf9ad
Davy Hausser <davy.hausser@gmail.com>
2020-05-25 02:08:52 +0200

    eslint project pass

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,6 @@ import { createLocalServer } from './server';
 
 const server = createLocalServer();
 
-console.log('test');
-
 server
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }: { url: string }) => {
